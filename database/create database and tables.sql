58aa971d1b1ca5dc5d829a2baa71be10548a684c
Ashton Graves <mail@ashtongraves.com>
2021-01-23 09:34:50 -0600

    updating database creation script

--- database/create database and tables.sql
+++ database/create database and tables.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE `apc_dev` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `apc_dev`;
 CREATE TABLE `questions` (
   `question_id` int NOT NULL AUTO_INCREMENT,
   `text` text NOT NULL,
@@ -14,7 +15,8 @@ CREATE TABLE `answers` (
   `y_value` int NOT NULL,
   `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `date_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`answer_id`)
+  PRIMARY KEY (`answer_id`),
+  FOREIGN KEY (`question_id`) REFERENCES `questions`(`question_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 CREATE TABLE `users` (
   `user_id` int NOT NULL AUTO_INCREMENT,
@@ -33,5 +35,7 @@ CREATE TABLE `users_answers` (
   `answer_id` int NOT NULL,
   `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `date_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`user_answer_id`)
+  PRIMARY KEY (`user_answer_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`),
+  FOREIGN KEY (`answer_id`) REFERENCES `answers`(`answer_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
